2f73cec2a15513910071025460a9e5c9c28d41b0
SalimmReza <91980956+SalimmReza@users.noreply.github.com>
2021-11-02 19:30:55 +0600

    half done

---
+++ app/src/main/java/com/example/my_dictionary/Model/Defination.java
@@ -0,0 +1,23 @@
+package com.example.my_dictionary.Model;
+
+public class Defination {
+
+    String definition ="";
+          String  example = "";
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+}
